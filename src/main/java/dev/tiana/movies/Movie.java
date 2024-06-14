8a19e9a747c00db090dd30b09b118ae01a4a4690
t4tiana <tianarenees@gmail.com>
2024-06-14 19:50:12 -0400

    first commit

---
+++ src/main/java/dev/tiana/movies/Movie.java
@@ -0,0 +1,24 @@
+package dev.tiana.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    private List<String> reviewIds;
+}
