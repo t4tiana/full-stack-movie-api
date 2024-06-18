1fb14d04bd1b27b1ae91ea2ad6dbfed7c39c3a6b
t4tiana <tianarenees@gmail.com>
2024-06-17 20:27:06 -0400

    Added fast and furious db data

--- src/main/java/dev/tiana/movies/Movie.java
+++ src/main/java/dev/tiana/movies/Movie.java
@@ -9,7 +9,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-@Document(collection = "movies")
+@Document(collection = "ff")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
