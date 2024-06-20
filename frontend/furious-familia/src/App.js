8fc36d1d175816e71630ccca359b9b9cf6609f07
t4tiana <tianarenees@gmail.com>
2024-06-20 18:47:16 -0400

    Add Home, Layout components and Routes

--- frontend/furious-familia/src/App.js
+++ frontend/furious-familia/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
@@ -12,16 +15,12 @@ function App() {
     try {
       const response = await api.get("/api/v1/movies");
 
-      console.log(response.data);
-
       setMovies(response.data);
-
     } 
     catch (err) 
     {
       console.log(err);
     }
-
   }
 
   /*Run getMovies function as soon as App loads */
@@ -31,6 +30,12 @@ function App() {
 
   return (
     <div className="App">
+
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home/>}></Route>
+        </Route>
+      </Routes>
 
     </div>
   );
---
+++ frontend/furious-familia/src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            Are you ready to live life a quarter mile at a time?
+        </div>
+    )
+}
+
+export default Home
