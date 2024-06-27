856b5ab27fff2095f2eee837a36f7fc7ff5f1b3d
t4tiana <tianarenees@gmail.com>
2024-06-27 13:40:14 -0400

    Style movie carousel

--- frontend/furious-familia/src/components/hero/Hero.js
+++ frontend/furious-familia/src/components/hero/Hero.js
@@ -11,13 +11,13 @@ const Hero = ({movies}) => {
                         return (
                             <Paper>
                             <div className='movie-card-container'>
-                                <div className='movie-card'>
+                                <div className='movie-card' style={{"--img": `url(${movie.backdrops[0]})`}}>
                                     <div className='movie-detail'>
                                         <div className='movie-poster'>
                                             <img src = {movie.poster} alt="Fast and Furious movie poster"/>
                                         </div>
                                         <div className='movie-title'>
-                                            <h4>{movie.title}</h4>
+                                            <h4 id='movie-title'>{movie.title}</h4>
                                         </div>
                                     </div>
                                 </div>
